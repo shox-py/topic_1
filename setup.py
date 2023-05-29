52185bd8ac2f0bd5ba2f1f5e3b6c1b31babbef9b
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-05-29 12:13:19 +0100

    Первое обновление
    - Добавлен теоретический материал темы
    - Добавлены задания в тему вместе с шаблонами для решения задач
    - Добавлен файл `.gitignore`
    - Добавлены файлы настроек проекта (`setup.cfg`, `setup.py`)

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="topic_1",
+    version="1.0.0",
+    author="Shoxrux Yuldashov",
+    description="1. Начало работы",
+)
---
+++ tasks/task_3.py
@@ -0,0 +1,9 @@
+string_literal = ...  # здесь Ваш код
+integer_literal = ...  # здесь Ваш код
+float_literal = ...  # здесь Ваш код
+boolean_literal = ...  # здесь Ваш код
+
+print(string_literal)
+print(integer_literal)
+print(float_literal)
+print(boolean_literal)
